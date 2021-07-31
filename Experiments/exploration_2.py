2c7577a0d30fca37567b07c7c6f0b12f1ed69089
root <root@LAPTOP-B1L5FNHD.localdomain>
2021-07-31 23:42:56 +0530

    Making soe edits

--- Experiments/exploration_2.py
+++ Experiments/exploration_2.py
@@ -7,11 +7,11 @@ from nltk.stem import PorterStemmer
 
 
 # Downloads
-nltk.download('stopwords')
-nltk.download('wordnet')
+# nltk.download('stopwords')
+# nltk.download('wordnet')
 
 def cleaning_function(data):
-    global to_remove, stopWords, lem
+    global to_remove, stopWords, lem, ps
 
     # print(f"{data}")
     if pd.isna(data):
@@ -28,7 +28,10 @@ def cleaning_function(data):
     stop_words_removed = [word.lower() for word in split_punc_removed if len(word) > 3 and word.lower() not in stopWords]
 
     # Lemmatizing words and forming new data
-    new_data = ' '.join([lem.lemmatize(word) for word in stop_words_removed])
+    #new_data = ' '.join([lem.lemmatize(word) for word in stop_words_removed])
+
+    # TRIAL -> with porter stemmer
+    new_data = ' '.join([ps.stem(word) for word in stop_words_removed])    
 
     return new_data
 
@@ -37,8 +40,8 @@ ps = PorterStemmer()
 lem = WordNetLemmatizer()
 stopWords = set(stopwords.words('english'))
 
-infile = r'E:\Amazon ML Challenge\dataset52a7b21\dataset\train.csv'
-outfile = r'E:\Amazon ML Challenge\dataset52a7b21\dataset\Processed\train2.csv'
+infile = r'/mnt/d/amazon-ml/dataset/train.csv'
+outfile = r'/mnt/d/amazon-ml/dataset/Processed/train3.csv'
 step = 10000
 to_remove = string.punctuation + string.digits
 
@@ -51,3 +54,11 @@ for i in range(10):
     df['BULLET_POINTS'] = df['BULLET_POINTS'].apply(cleaning_function)
 
     df.to_csv(outfile, mode='w' if i==0 else 'a', header=True if i==0 else False, index=False)
+
+
+
+"""
+train2 -> lemmetization
+train3 -> porter stemmer
+
+"""
--- Experiments/freq_match_1.py
+++ Experiments/freq_match_1.py
@@ -17,6 +17,7 @@ def score_individual_group(row_words, group_words):
     for word in row_words:
         if word in group_words:
             score += group_words[word] * row_words[word]
+            # score += row_words[word]
 
     # print(f"Group words is\n{group_words}")
     denominator = sum(group_words.values())
@@ -59,6 +60,8 @@ def get_label_for_row(row):
 
 def test_predictions(test_data, num_of_rows):
     # Counts accuracy as well
+    global group_word_freq, column_used
+
     rows_predicted_correctly = 0
     null_values = 0
 
@@ -68,9 +71,16 @@ def test_predictions(test_data, num_of_rows):
     for i in range(num_of_rows):
 
         # print(f"Predicting for\n{test_data.iloc[i]}")
-        predicted_group_label = get_label_for_row(test_data.iloc[i])
+        predicted_group_label= get_label_for_row(test_data.iloc[i])
         if predicted_group_label == test_data.iloc[i]['BROWSE_NODE_ID']:
             rows_predicted_correctly += 1
+        # else:
+        #     actual_group_label = test_data.iloc[i]['BROWSE_NODE_ID']
+        #     row_word_freq = FreqDist(nltk.word_tokenize(test_data.iloc[i][column_used]))
+        #     print(f"\nPREDICTED SCORE : {score}   ACTUAL SCORE {score_individual_group(row_word_freq, group_word_freq[actual_group_label])}")
+        #     print(f"predicted group {predicted_group_label}\nactual group {actual_group_label}\ntest data : {test_data.iloc[i]}")
+        #     console.print(f'predict word freq {group_word_freq[predicted_group_label]}\nactual word freq : {group_word_freq[actual_group_label]}')
+        #     break
 
         if predicted_group_label == -1:
             null_values += 1
@@ -88,7 +98,7 @@ def test_predictions(test_data, num_of_rows):
 pd.set_option("display.max_rows", None, "display.max_columns", None, "display.max_colwidth", 200)
 console = Console()
 
-datapath = r'E:\Amazon ML Challenge\dataset52a7b21\dataset\Processed\train.csv'
+datapath = r'/mnt/d/amazon-ml/dataset/Processed/train3.csv'
 data = pd.read_csv(datapath, nrows=100000)
 train, test = train_test_split(data, test_size=0.2)
 node_grp = train.groupby('BROWSE_NODE_ID', axis='index')
@@ -97,7 +107,77 @@ column_used = 'TITLE'
 top_n_group_words = 20
 
 sorted_words = node_grp[column_used].apply(lambda series: sorted(FreqDist(nltk.word_tokenize(series.str.cat(sep=' '))).items(), key=lambda k: k[1], reverse=True)[:top_n_group_words])
+
+#FreqDist(nltk.word_tokenize(series.str.cat(sep=' '))).items(), key=lambda k: k[1], reverse=True)
+
+
 group_word_freq = sorted_words.apply(lambda row: {key: val for key, val in row}).to_dict()
-# console.print(list(group_word_freq.items())[:20])
+#console.print(list(group_word_freq.items())[:20])
 
 test_predictions(test, 10000)
+
+
+'''
+with column -> TITLE
+scoring -> weighted average  with lemmetization               38%
+scoring -> direct addition with lemmetization                 42%
+scoring -> multiply with weight and add with lemmetization    21%
+scoring -> direct addition with porter stemmer                48%
+scoring -> weighted average with porter stemmer               39-40%             
+
+
+
+problems with scoring
+
+1 -> 
+2 ->
+3 ->
+
+problems with pre-processing
+
+
+
+6 102*1 ->
+
+7 2*5 -> 
+
+6 -> []
+
+[gismo oneplus back cover printed designer soft case oneplus design]
+[0 0 20 0 19 18] -> [] 
+
+5000    
+40  50  60
+3   4   2
+
+'cover': 6359, 'back': 6012, 'case': 4656, 'printed': 3377,
+'designer': 2511, 'hard': 2100, 'samsung': 1453, 'galaxy': 1357, 'mobile': 1245, 'soft': 1086, 'redmi': 921, 'vivo':
+888, 'black': 812, 'design': 798, 'note': 720, 'oppo': 708, 'plus': 636, 'xiaomi': 609, 'iphone': 579, 'silicone': 560
+
+flipflops / flipflop / flip flop -> ngram to battle this
+
+{flip, lipf, ipfl, }
+
+4 ->1
+5 ->2
+6 ->3
+
+one plus/oneplus 
+
+{'oneplus': 2, 'back': 2, 'cover': 2, 'case': 2, 'mobbysol®': 1, 'colour': 1, 'full': 1, 'protective':
+1, 'plus': 1, 'five': 1, 'black': 1}, 
+actual word freq : {'cover': 6359, 'back': 6012, 'case': 4656, 'printed': 3377,
+'designer': 2511, 'hard': 2100, 'samsung': 1453, 'galaxy': 1357, 'mobile': 1245, 'soft': 1086, 'redmi': 921, 'vivo':
+888, 'black': 812, 'design': 798, 'note': 720, 'oppo': 708, 'plus': 636, 'xiaomi': 609, 'iphone': 579, 'silicone': 560}
+
+
+
+degre full bodi protect front back case cover ipaki style vivo gold
+
+
+actual ->  2 
+predicted -> 1
+
+
+
+'''
